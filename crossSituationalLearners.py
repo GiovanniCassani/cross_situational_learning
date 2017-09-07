5adbdbcb6fc57ef70b0c87d7df8fdee5334e8489
Giovanni Cassani <cassani.giovanni@gmail.com>
2017-09-07 14:00:52 +0200

    python3 (plus minor fixes)
    Updated code to Python3 and adjusted some minor issues

--- crossSituationalLearners.py
+++ crossSituationalLearners.py
@@ -48,8 +48,8 @@ def print_cue_outcome_associations(avg_associations, n):
     for k1 in sorted(avg_associations.keys()):
         for k2 in sorted(avg_associations[k1].keys()):
             # print each cue-outcome pair with the corresponding association and its standard deviation
-            print '\t'.join([k1, k2, str(round(avg_associations[k1][k2][0], n)),
-                             str(round(avg_associations[k1][k2][1], n))])
+            print('\t'.join([k1, k2, str(round(avg_associations[k1][k2][0], n)),
+                             str(round(avg_associations[k1][k2][1], n))]))
 
 
 ########################################################################################################################
@@ -153,6 +153,7 @@ def compute_total_activation(matrix, outcome, trial_cues, lam):
     :param outcome:     a string indicating the outcome being considered
     :param trial_cues:  a dictionary containing the cues that are present in the current learning trial as keys, and
                         the number of times each cue occurs as values
+    :param lam:         the maximum amount of activation an outcome can sustain from all the afferent cues
     :return v_total:    the total activation that goes from the cues that are present in the learning trial to the
                         outcome being considered
     """
@@ -170,10 +171,10 @@ def compute_total_activation(matrix, outcome, trial_cues, lam):
     # lambda parameter specifies the maximum amount of activation each outcome can bear, a v_total that is higher than
     # lambda corresponds to an over-prediction of the presence of the outcome which causes the model to collapse.
     if v_total > lam:
-        print trial_cues, outcome
-        print v_total
-        raise ValueError('Something went wrong! The total amount of activation cannot exceed one: ' +
-                         'try lowering the learning rate, alpha, or the cue salience parameter, beta')
+        print(trial_cues, outcome)
+        print(v_total)
+        raise ValueError('Something went wrong! The total amount of activation cannot exceed %d: ' % lam +
+                         'try lowering the learning rate, alpha, or the cue salience parameter, beta.')
     else:
         return v_total
 
@@ -201,9 +202,8 @@ def update_outcome_weights(matrix, outcome, trial_cues, trial_outcomes, lam, ab)
     v_total = compute_total_activation(matrix, outcome, trial_cues, lam)
 
     # if the outcome doesn't occur in the current trial, set the value of lambda to 0
-    if outcome not in trial_outcomes:
-        lam = 0
-    delta_v = ab * (lam - v_total)
+    l = lam if outcome in trial_outcomes else 0
+    delta_v = ab * (l - v_total)
 
     # for each cue from the current learning trial, update its association to the outcome being considered
     for cue in trial_cues:
@@ -212,9 +212,9 @@ def update_outcome_weights(matrix, outcome, trial_cues, trial_outcomes, lam, ab)
         except KeyError:
             matrix[outcome][cue] = delta_v
 
-        if matrix[outcome][cue] > 1:
-            print outcome, cue, matrix[outcome][cue]
-            raise ValueError('An outcome can only sustain an activation of 1 and it looks like the ' +
+        if matrix[outcome][cue] > lam:
+            print(outcome, cue, matrix[outcome][cue])
+            raise ValueError('An outcome can only sustain an activation of %d and it looks like the ' % lam +
                              'activation from a single cue is higher than this threshold. Something ' +
                              'went wrong, try lowering the learning rate, alpha, or the cue salience ' +
                              'parameter, beta.')
@@ -225,7 +225,7 @@ def update_outcome_weights(matrix, outcome, trial_cues, trial_outcomes, lam, ab)
 ########################################################################################################################
 
 
-def discriminative_learner(training_trials, iterations, alpha=0.2, beta=0.1, lam=1,
+def discriminative_learner(training_trials, iterations, alpha=0.1, beta=0.1, lam=1,
                            seed=None, print_output=False):
 
     """
@@ -262,7 +262,7 @@ def discriminative_learner(training_trials, iterations, alpha=0.2, beta=0.1, lam
 
     rnd.seed(seed)
 
-    for i in xrange(iterations):
+    for i in range(iterations):
 
         # the order of presentation impacts learning, so training trials are randomly shuffled before each simulation
         rnd.shuffle(training_trials)
@@ -280,7 +280,8 @@ def discriminative_learner(training_trials, iterations, alpha=0.2, beta=0.1, lam
             #
             for outcome in outcomes:
 
-                ith_associations = update_outcome_weights(ith_associations, outcome, trial_cues, trial_outcomes, lam, ab)
+                ith_associations = update_outcome_weights(ith_associations, outcome, trial_cues,
+                                                          trial_outcomes, lam, ab)
 
         # store association scores at the end of the current iteration
         store_current_associations(ith_associations, associations)
@@ -384,7 +385,7 @@ def hypothesis_testing_model(training_trials, iterations, alpha=0.6, alpha_1=(0.
 
     rnd.seed(seed)
 
-    for i in xrange(iterations):
+    for i in range(iterations):
 
         ith_hypotheses = {}                                                     # contains formed hypothesis
         ith_recalled = defaultdict(dict)                                        # contains recalled hypothesis
@@ -403,13 +404,15 @@ def hypothesis_testing_model(training_trials, iterations, alpha=0.6, alpha_1=(0.
             if outcome not in ith_hypotheses:                               # IF it is new
                 sel = rnd.randint(1, len(trial_cues)) - 1                     # select a cue at random
                 ith_hypotheses[outcome] = trial_cues[sel]                     # form a cue-outcome hypothesis
+
             else:                                                           # IF it is in memory
                 if outcome not in ith_recalled:                               # BUT encountered just once
                     p = alpha
                     recall = np.random.binomial(1, p)                           # recall with probability alpha
                 else:                                                         # IF it was encountered more times
-                    if len(alpha_1) >= ith_recalled[outcome]:
-                        p = alpha_1[ith_recalled[outcome]]                      # recall with increased alpha
+                    k = list(ith_recalled[outcome].keys())[0]                   # find how many times the cue-outcome
+                    if len(alpha_1) >= ith_recalled[outcome][k]:                     # hypothesis was already retrieved
+                        p = alpha_1[ith_recalled[outcome][k]-1]                      # recall with increased alpha
                     else:
                         p = alpha_1[-1]                                         # BUT never more than .99
                     recall = np.random.binomial(1, p)
@@ -440,7 +443,7 @@ def hypothesis_testing_model(training_trials, iterations, alpha=0.6, alpha_1=(0.
         for c in hypotheses[o]:
             avg_hypotheses[o][c] = (hypotheses[o][c] / float(iterations), 0)
             if print_output:
-                print o, c, avg_hypotheses[o][c]
+                print(o, c, avg_hypotheses[o][c])
 
     return avg_hypotheses
 
@@ -482,7 +485,7 @@ def probabilistic_learner(training_trials, iterations, t0_prob=10 ** -4, beta=10
 
     rnd.seed(seed)
 
-    for i in xrange(iterations):
+    for i in range(iterations):
 
         outcomes = set()
         ith_outcomes_given_cues = defaultdict(dict)
@@ -563,13 +566,13 @@ def probabilistic_learner(training_trials, iterations, t0_prob=10 ** -4, beta=10
 ########################################################################################################################
 
 
-def cross_situational_learning(input_file, n_iter=200, seed=6, print_output=False):
+def cross_situational_learning(in_file, i=200, s=6, print_output=False):
 
     """
-    :param input_file:          a .txt file containing two columns. The first column is assumed to contain cues, while
+    :param in_file:          a .txt file containing two columns. The first column is assumed to contain cues, while
                                 the second columns is assumed to contain outcomes.
-    :param n_iter:              an integer stating how many iterations need to be performed to evaluate each learner
-    :param seed:                an integer to seed the random permutation of learning trials in the training set, for
+    :param i:              an integer stating how many iterations need to be performed to evaluate each learner
+    :param s:                an integer to seed the random permutation of learning trials in the training set, for
                                 reproducibility
     :param print_output:        a boolean specifying whether average associations should be printed after having been
                                 computed
@@ -580,17 +583,16 @@ def cross_situational_learning(input_file, n_iter=200, seed=6, print_output=Fals
                                 iterations between a cue and an outcome, and its standard deviation.
     """
 
-    training_trials = read_input_trials(input_file)
+    training_trials = read_input_trials(in_file)
 
-    learning_outcomes = defaultdict(dict)
+    outcomes = defaultdict(dict)
 
-    learning_outcomes['ndl'] = discriminative_learner(training_trials, n_iter, seed=seed, print_output=print_output)
-    learning_outcomes['HebbianLearner'] = hebbian_learner(training_trials, print_output=print_output)
-    learning_outcomes['probabilisticLearner'] = probabilistic_learner(training_trials, n_iter, seed=seed,
-                                                                      print_output=print_output)
-    learning_outcomes['HTM'] = hypothesis_testing_model(training_trials, n_iter, print_output=print_output)
+    outcomes['ndl'] = discriminative_learner(training_trials, i, seed=s, print_output=print_output)
+    outcomes['HebbianLearner'] = hebbian_learner(training_trials, print_output=print_output)
+    outcomes['probabilisticLearner'] = probabilistic_learner(training_trials, i, seed=s, print_output=print_output)
+    outcomes['HTM'] = hypothesis_testing_model(training_trials, i, print_output=print_output)
 
-    return learning_outcomes
+    return outcomes
 
 
 ########################################################################################################################
@@ -613,16 +615,16 @@ def main():
     if not os.path.exists(args.input_file):
         raise ValueError("The path you provided does not exist. Please, provide the path to an existing file.")
     else:
-        learning_outcomes = cross_situational_learning(args.input_file, n_iter=args.n_iter,
-                                                       seed=args.seed, print_output=args.out)
+        learning_outcomes = cross_situational_learning(args.input_file, i=args.n_iter,
+                                                       s=args.seed, print_output=args.out)
 
     for algorithm in learning_outcomes:
-        print algorithm
-        print "\tOutcome-Cue: Mean (stdev)"
+        print(algorithm)
+        print("\tOutcome-Cue: Mean (stdev)")
         for w1 in learning_outcomes[algorithm]:
             for w2 in learning_outcomes[algorithm][w1]:
                 mean, stdev = learning_outcomes[algorithm][w1][w2]
-                print "\t"+"-".join([w1,w2])+": "+str(mean)+' ('+str(stdev)+')'
+                print("\t" + "-".join([w1, w2]) + ": " + str(mean) + ' ('+str(stdev) + ')')
 
 ########################################################################################################################
 
